ca8e7e8ba66209f7a6f2d04f1ec424047ba24b4e
Dilan <dilandarren1@gmail.com>
2024-09-16 22:22:56 +0200

    add pros

--- vuejs/reference-guide.js
+++ vuejs/reference-guide.js
@@ -21,17 +21,40 @@
 
 
  Script <script></script>
+
+
+
+
+//EMITS(envio solamente funciones)
+
+-En el nombre de la funcion del padre se escribe en camelCase y con el signo '@'. 
+    Ex : 
+    1) Declaracion de la funcion: const definirPresupuesto = () => {Cuerpo de la funcion/ Codígo}.
+    2)Declaracion de la funcion: En el componente: @definir-presupuesto="definirPresupuesto"
+
+-En el nombre de la funcion del hijo se escribe con '-' y se revibe en el defineEmits: 
+    Ex:
+    1) se recibe la funcion: const emit = defineEmits(['ocultar-modal']). 
+    2) En la etiqueta: @ocultar-modal="ocultarModal"
+
+
+
+PROPS(envio solamente variables):
+
+En el PADRE se escribe igual que una variable signo ':'.Ex: :unaVariable="unaVariable".
+
+En el HIJO se escriben con el 'defineProps()'.
+    Ex:
+    const props = defineProps({
+        unaVariable:{
+            type:Object,
+            required: true
+        }
+    })
  */
 
 
 
-//EMITS
-
-
-
-//-En el nombre de la funcion del padre se escribe en camelCase. Ex : 1) Declaracion de la funcion: const definirPresupuesto = () => {Cuerpo de la funcion/ Codígo}.2)Declaracion de la funcion: En el componente: @definir-presupuesto="definirPresupuesto"
-
-//-En el nombre de la funcion del hijo se escribe con '-', Ex: 1) se recibe la funcion: const emit = defineEmits(['ocultar-modal']). 2) En la etiqueta: @ocultar-modal="ocultarModal"
 
 
 
