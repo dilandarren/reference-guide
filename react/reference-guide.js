d9c4ff2a473aa9a5c7274cbcdca2f708e8503a43
dilandarren <dilandarren@gmail.com>
2024-12-23 21:46:35 +0100

    test

---
+++ react/reference-guide.js
@@ -0,0 +1,44 @@
+
+
+
+/***********************************************************************
+ * 
+ * Variables
+ * 
+ ***********************************************************************/
+
+
+
+
+
+
+
+
+
+
+
+/* 
+
+Hooks
+
+
+se Importa:
+import { useState } from "react"
+
+
+Luego en el componente:
+const [variable, setVariable] = useState('valor inicial')        //Se puede inicializar con cualquier valor
+
+
+
+
+*/
+
+
+
+
+
+
+// ------------------------------------------------------------------
+//     ----------- Template <template></template> -----------
+// ------------------------------------------------------------------
