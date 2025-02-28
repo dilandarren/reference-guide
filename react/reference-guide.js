e27aa1ff8d39d809e45beb82f5789359ad6aa86f
dilandarren <dilandarren@gmail.com>
2025-02-28 22:18:42 +0100

    reference-react

--- react/reference-guide.js
+++ react/reference-guide.js
@@ -19,7 +19,7 @@
 
 /* 
 
-Hooks
+UseState:
 
 
 se Importa:
@@ -32,13 +32,42 @@ const [variable, setVariable] = useState('valor inicial')        //Se puede inic
 
 
 
+
+
+
+
+
+
+
+------------------------------------------------------------------
+     -----------  -----------
+ ------------------------------------------------------------------
+
+
+
+ ----------Estructura De Proyecto De JSX---------------
+
+ import React from 'react'          //<--------- aqui van los import
+
+ cons formData = {        //<--------- (NO SIEMPRE)van las declaraciones de las variables y/o constantes y/o funciones
+    ...
+ }
+
+ export const reference-guide = () => {
+
+
+    const dispatch = useDispatch(); //<--------- Aqui van los "use..." (Hook)
+
+
+        const formData = {        //<--------- (NO SIEMPRE)van las declaraciones de las variables y/o constantes y/o funciones
+            ...
+        }
+
+
+    return (
+        <div>reference-guide</div>  //aqui va el HTML y/o renderisaciones y/o llamadas a componentes
+    )
+ }
+
 */
 
-
-
-
-
-
-// ------------------------------------------------------------------
-//     ----------- Template <template></template> -----------
-// ------------------------------------------------------------------
